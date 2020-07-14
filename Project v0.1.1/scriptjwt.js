eaea794b8b6c138b08ecf579349325c2c47bc188
llemrabott52 <42483583+llemrabott52@users.noreply.github.com>
2020-07-14 18:33:08 +0100

    script to generate Tokens

---
+++ Project v0.1.1/scriptjwt.js
@@ -0,0 +1,12 @@
+function generate_token(length){
+    //edit the token allowed characters
+    var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("");
+    var b = [];  
+    for (var i=0; i<length; i++) {
+        var j = (Math.random() * (a.length-1)).toFixed(0);
+        b[i] = a[j];
+    }
+    return b.join("");
+}
+var toks = generate_token(32); //returns "qweQj4giRJSdMNzB8g1XIa6t3YtRIHPH"
+console.log(toks);

0b7d25e24ebc18b7dc85b3bfbf7271cc58a4c407
llemrabott52 <42483583+llemrabott52@users.noreply.github.com>
2020-07-20 18:57:10 +0100

    Add files via upload

---
+++ Project v0.1.2/pages/login.tsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import Login from '../components/Login/Login'; 
+
+
+const login: React.FC = () => {
+
+  return (
+    <div className="container">
+      <Login />
+    </div>
+    
+  );
+};
+
+
+
+export default login;

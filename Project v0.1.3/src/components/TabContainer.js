f8899e1e0fa63e6f0ce81cb4222f7163870e28ca
llemrabott52 <42483583+llemrabott52@users.noreply.github.com>
2020-07-23 18:05:16 +0000

    Add files via upload

---
+++ Project v0.1.3/src/components/TabContainer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import TabOnePage from "../pages/TabOnePage";
+import TabTwoPage from "../pages/TabTwoPage";
+import { IonTabs } from "@ionic/react";
+
+const TabContainer = ({ history, changedTabs, addItem , showAddItemModal}) => {
+  return (
+    <ion-tabs ionTabsDidChange={e => { changedTabs(e); }}>
+      <ion-tab tab="tab1">
+        <TabOnePage history={history} addItem={addItem} showAddItemModal={showAddItemModal}/>
+      </ion-tab>
+      <ion-tab tab="tab2">
+        <TabTwoPage history={history} />
+      </ion-tab>
+
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" onClick={changedTabs}>
+          <ion-label>Home</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab2" onClick={changedTabs}>
+          <ion-label>Settings</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  );
+};
+
+export default TabContainer;

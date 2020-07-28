ec8c92165486c07f316ba22a8c3c96d4390eac1a
llemrabott52 <42483583+llemrabott52@users.noreply.github.com>
2020-07-28 19:01:11 +0100

    Add files via upload

---
+++ localstorage/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+import FormDataComponent from './components/form-data.component';
+
+class  App extends Component {
+  render(){
+    return (
+      <div className="App">
+          <FormDataComponent />
+      </div>
+    );
+  }
+  
+}
+
+export default App;

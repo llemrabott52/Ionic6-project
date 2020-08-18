cf278d7a8363fe352cdeccb701686ed460314787
llemrabott52 <42483583+llemrabott52@users.noreply.github.com>
2020-08-18 18:19:01 +0100

    Add files via upload

---
+++ Helper/ListUtilisateur.java
@@ -0,0 +1,21 @@
+package com.monetique.dto;
+
+import java.util.List;
+
+import com.monetique.models.Utilisateur;
+
+public class ListUtilisateur {
+
+	List<Utilisateur> list;
+
+	public List<Utilisateur> getList() {
+		return list;
+	}
+
+	public void setList(List<Utilisateur> list) {
+		this.list = list;
+	}
+	
+	
+	
+}
